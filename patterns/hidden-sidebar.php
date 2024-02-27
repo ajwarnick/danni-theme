e4b90bdd07af11f128708dc1d05b7734e204a018
Anthony Warnick <github@warnick.co>
2024-02-26 20:29:35 -0600

    add sidebar

---
+++ patterns/hidden-sidebar.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Sidebar
+ * Slug: twentytwentyfour/hidden-sidebar
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}, "style":{"spacing":{"blockGap":"36px","padding":{"right":"0","left":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4,"layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group -->
